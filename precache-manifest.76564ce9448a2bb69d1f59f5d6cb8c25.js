ba0de50a954956acb90c10fc25b3455435d11fee
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-12-02 11:10:49 +0000

    Updates

---
+++ precache-manifest.76564ce9448a2bb69d1f59f5d6cb8c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac87b156b9bc73b1d9d318f917f27562",
+    "url": "/Dice-Game-React/index.html"
+  },
+  {
+    "revision": "164a8107d0ba7fa26850",
+    "url": "/Dice-Game-React/static/css/main.62606043.chunk.css"
+  },
+  {
+    "revision": "fd256c78a84b4bbb6670",
+    "url": "/Dice-Game-React/static/js/2.d3d76963.chunk.js"
+  },
+  {
+    "revision": "164a8107d0ba7fa26850",
+    "url": "/Dice-Game-React/static/js/main.0d3446e9.chunk.js"
+  },
+  {
+    "revision": "a3eee8f7b0c81080f37b",
+    "url": "/Dice-Game-React/static/js/runtime-main.987142f4.js"
+  },
+  {
+    "revision": "80c2898fd85314c171515bac0c53bba1",
+    "url": "/Dice-Game-React/static/media/win.80c2898f.png"
+  }
+]);
